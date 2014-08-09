9bea6af0873b7e88bc8a941b440ef38f748e3224
Luke Pommersheim <lpommers@gmail.com>
2014-08-09 13:54:44 +0200

    updated the most recent stories bit

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,8 +10,9 @@
 angular.module('nprApiProjectApp')
   .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
   	$scope.news = [];
-  	$http.get('https://api.npr.org/query?date=current&output=JSON&apiKey=MDE1ODE3ODQzMDE0MDczMjkzNDU1ZGE0NQ001').success(function(data){
+  	$http.get('https://api.npr.org/query?&output=JSON&apiKey=MDE1ODE3ODQzMDE0MDczMjkzNDU1ZGE0NQ001').success(function(data){
   		$scope.news = data.list.story;
-  		console.log($scope.news)
+  		console.log($scope.news);
   	});
   }]);
+
